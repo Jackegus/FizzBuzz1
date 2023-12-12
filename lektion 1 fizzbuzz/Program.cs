f2e8b588332f7377f980b945407aa8ad3827c2f1
Jakob <Jakob@DESKTOP-QP5IE94>
2023-12-12 14:23:13 +0100

    Add project files.

---
+++ lektion 1 fizzbuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FizzBuzz(100);
+
+        void FizzBuzz(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+
+}
